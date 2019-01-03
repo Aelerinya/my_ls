14b571ac32f2f30b59a7d419144a59b55bd41d5a
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2019-01-03 20:41:00 +0100

    Functional my_ls without arguments
    Without arguments, my_ls lists the content of the current directory,
      sorted in "alphabetic" order

--- src/main.c
+++ src/main.c
@@ -5,9 +5,18 @@
 ** main function
 */
 
+#include <stdio.h>
 #include "prototypes.h"
 
-int main(void)
+int main(int ac, char **av)
 {
+    char *options = get_options(ac, av);
+    char **files;
+
+    if (ac == 1) {
+        files = read_file_names(".");
+        bubble_sort(files, &my_strcmp);
+        display_list(files);
+    }
     return (0);
 }
